69569447fbc2c6131e20fa56f881889daba12873
Walace Nascimento <wlacens@hotmail.com>
2026-03-10 20:54:34 -0300

    Configuração do servidor e rota de teste

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Testando a aplicação!");
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Aqui será definido as rotas
+app.get("/", (req, res) => {
+    res.status(200).json({
+        Ok: true,
+        Mensagem: "Rota GET/ primeira rota da API-PETS"
+    })
+});
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
